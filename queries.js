4a62ae848353c5deb5d135a0ecc00ca3bd03d9eb
Tahseen <tahseenislam@hotmail.com>
2023-03-29 23:14:56 +1000

    added initial auth

--- queries.js
+++ queries.js
@@ -1,4 +1,5 @@
 const { password, user, host, database } = require('./config.js');
+const bcrypt = require('bcryptjs');
 
 const Pool = require('pg').Pool;
 const pool = new Pool({
@@ -30,12 +31,14 @@ const getUserById = (request, response) => {
   });
 };
 
-const createUser = (request, response) => {
-  const { name, email } = request.body;
+const createUser = async (request, response) => {
+  const { username, password } = request.body;
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
 
   pool.query(
-    'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
-    [name, email],
+    'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *',
+    [username, hash],
     (error, results) => {
       if (error) {
         throw error;
@@ -43,19 +46,21 @@ const createUser = (request, response) => {
       response
         .status(201)
         .send(
-          `User (${name}) with email (${email}) added with ID: ${results.rows[0].id}`
+          `User (${username}) with email (${username}) added with ID: ${results.rows[0].id}`
         );
     }
   );
 };
 
-const updateUser = (request, response) => {
+const updateUser = async (request, response) => {
   const id = parseInt(request.params.id);
-  const { name, email } = request.body;
+  const { username, password } = request.body;
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
 
   pool.query(
     'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-    [name, email, id],
+    [username, hash, id],
     (error, results) => {
       if (error) {
         throw error;
@@ -74,6 +79,13 @@ const deleteUser = (request, response) => {
     }
     response.status(200).send(`User deleted with ID: ${id}`);
   });
+};
+
+const emailExists = async (email) => {
+  const data = await pool.query('SELECT * FROM users WHERE email=$1', [email]);
+  if (data.rowCount == 0) return false;
+  console.log(data.rowCount);
+  return data.rows[0];
 };
 
 module.exports = {
